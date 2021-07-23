6157ccf6164a53e43d7e053b2d39b0042c50a65d
huangyi <245807235@qq.com>
2021-07-23 13:38:06 +0800

    config change

--- nav.config.ts
+++ nav.config.ts
@@ -11,19 +11,19 @@ const c: IConfig = {
   hashMode: true,
 
   // 是否显示右上角的 Github 图标
-  showGithub: true,
+  showGithub: false,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://baidu.com',
+  homeUrl: '',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '自用导航',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '自用导航',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '自用',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
   theme: 'Light',
@@ -31,7 +31,6 @@ const c: IConfig = {
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
   `,
 
   // 阿里巴巴矢量图库 https://www.iconfont.cn/
@@ -52,7 +51,7 @@ const c: IConfig = {
     posterImageUrls: [
       'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
     ],
-    description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
+    description: '收录<b>${total}</b>个网站'
   }
 }
 
